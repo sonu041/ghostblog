2ab5ced3c3f4992260cc044c574429d989c06169
sonu041 <sonu041@gmail.com>
2014-04-06 18:53:21 +0530

    Add postgres database for heroku

--- config.js
+++ config.js
@@ -51,9 +51,13 @@ config = {
         url: 'http://shuvablog.herokuapp.com',
         mail: {},
         database: {
-            client: 'sqlite3',
+            client: 'postgres',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
+                host: process.env.POSTGRES_HOST,
+                user: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                database: process.env.POSTGRES_DATABASE,
+                port: '5432'
             },
             debug: false
         },
